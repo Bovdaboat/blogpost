a4f69f136818a2b6e7c15f57f69a772450bc6826
bovdaboat <ignatgabov@gmail.com>
2023-05-23 17:43:19 -0700

    adding next js links

--- app/(shared)/Navbar.tsx
+++ app/(shared)/Navbar.tsx
@@ -1,10 +1,14 @@
 import React from "react"
+import Link from "next/link";
 
 const Navbar = () => {
     return(
         <header className="mb-5">
             <nav className="flex justify-between items-center w-full bg-wh-900 text-wh-10 px-10 PY-4">
-                <div className="hidden sm:">social links</div>
+                <div className="hidden sm:block">social links</div>
+                <Link href='/'>Home</Link>
+                <Link href='/'>Trending</Link>
+                <Link href='/'>About</Link>
             </nav>
         </header>
     )
