5eda001f0efdd92ad852ac72e47e43a6d4694ff1
bovdaboat <ignatgabov@gmail.com>
2023-05-25 21:21:14 -0700

    added blog of tomorrow

--- app/(shared)/Footer.tsx
+++ app/(shared)/Footer.tsx
@@ -3,7 +3,17 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer className='bg-wh-900 text-wh-50 py-10 px-10'>
+            <div className="justify-between mx-auto gap-16 sm:flex">
+                {/* first column */}
+                <div className="mt-16 basis-1/2 sm:mt-0">
+                    <h4 className="font-bold">
+                        Blog of Tomorrow
+                    </h4>
+
+                </div>
+            </div>
+        </footer>
     )
 }
 
