2af0dd13c8bdf7e8ed134b539ca292a4d89a032c
bovdaboat <ignatgabov@gmail.com>
2023-05-26 22:14:20 -0700

    added botch of commpoments

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
-
+import {Tenali_Ramakrishna} from "next/dist/compiled/@next/font/dist/google";
 
 export default function Home() {
   return (
-    <main>
-      hi
+    <main className="px-10 leading-7">
+      <Trending />
+        <div className="md:flex gap-10 mb-5">
+            <div className="basis-3/4">
+                <Tech />
+                <Travel />
+                <Other />
+                <div className="hidden md:block">
+                    <Subscibe />
+                </div>
+            </div>
+            <div className="basis-1/4">
+                <Sidebar />
+            </div>
+        </div>
     </main>
   )
 }
