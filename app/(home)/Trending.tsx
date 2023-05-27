523a271634586901d254527cba3b1fe27332a47b
bovdaboat <ignatgabov@gmail.com>
2023-05-27 16:43:26 -0700

    added a trending line of code

---
+++ app/(home)/Trending.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+
+const Trending = () => {
+    return (
+        <section className='pt-3 pb-10'>
+            <div className="flex items-center gap-3">
+                <div className="bg-wh-900 py-2 px-8 text-wh-10 text-sm font-bold">
+            Treading
+                </div>
+                <p className="text-sm">
+                    Be the first to be amazed of how cool this all is, and dont forgent to add your favorite take 👀
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default Trending
