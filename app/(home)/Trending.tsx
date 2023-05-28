be4ee2dbb74fbcf341b1c7b486afff0d979ffff2
bovdaboat <ignatgabov@gmail.com>
2023-05-27 17:14:15 -0700

    using grid in the Trending.tsx file, very exited about it because it worked out really good

--- app/(home)/Trending.tsx
+++ app/(home)/Trending.tsx
@@ -9,9 +9,16 @@ const Trending = () => {
             Treading
                 </div>
                 <p className="text-sm">
-                    Be the first to be amazed of how cool this all is, and dont forgent to add your favorite take 👀
+                    Be the first to be amazed of how cool this all is, and dont forget to add your favorite take 👀
                 </p>
             </div>
+                <div className="sm:grid gap-5 grid-cols-4 grid-rows-2 sm:h-[600px] my-3">
+                    <div className='col-span-2 row-span-2 bg-wh-500'></div>
+                    <div className='col-span-2 row-span-1 bg-wh-500'></div>
+                    <div className='col-span-1 row-span-1 bg-wh-500'></div>
+                    <div className='col-span-1 row-span-1 bg-wh-500'></div>
+
+                </div>
         </section>
     )
 }
