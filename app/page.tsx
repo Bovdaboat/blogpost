56a04df86ee66f75108200e9b218ef0f8524aa70
bovdaboat <ignatgabov@gmail.com>
2023-05-28 23:55:02 -0700

    adding a Tech file

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Trending from "@/app/(home)/Trending";
+import Tech from "@/app/(home)/Tech";
 export default function Home() {
   return (
     <main className="px-10 leading-7">
       <Trending />
         <div className="md:flex gap-10 mb-5">
             <div className="basis-3/4">
-
+                <Tech />
                 <div className="hidden md:block">
 
                 </div>
