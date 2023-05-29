e4cc9ae0115dd3049e4c7e2374c66660a7266de3
bovdaboat <ignatgabov@gmail.com>
2023-05-29 12:42:44 -0700

    addid grid also need to make props

--- app/(home)/Tech.tsx
+++ app/(home)/Tech.tsx
@@ -8,8 +8,15 @@ const Tech = (props: Props) => {
             <hr className="border-1" />
             <div className="flex items-center gap-3 my-8">
                 <h4 className="bg-accent-orange py-2 px-5 text-wh-900 text-sm font-bold">
-                    It was it is
+                    Hot
                 </h4>
+                <p className="font-bold text-2xl">Latest new Technology's</p>
+                    <div className="sm:grid grid-cols-2 grid-rows-3 gap-x-8 gap-y-8 my-5">
+                        <div className="bg-wh-500 col-span-1 row-span-3"></div>
+                        <div className="bg-wh-500 col-span-1 row-span-1"></div>
+                        <div className="bg-wh-500 col-span-1 row-span-1"></div>
+                        <div className="bg-wh-500 col-span-1 row-span-1"></div>
+                    </div>
             </div>
         </section>
     )
