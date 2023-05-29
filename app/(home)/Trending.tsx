664fd43f345d6d786c884a85a20c9cf4416f87e5
bovdaboat <ignatgabov@gmail.com>
2023-05-28 17:26:55 -0700

    replaced the div with the TrendingCard on the Trending.tsx

--- app/(home)/Trending.tsx
+++ app/(home)/Trending.tsx
@@ -40,10 +40,10 @@ const Trending = (Props:string) => {
                 </p>
             </div>
                 <div className="sm:grid gap-5 grid-cols-4 grid-rows-2 sm:h-[600px] my-3">
-                    <div className='col-span-2 row-span-2 bg-wh-500'></div>
-                    <div className='col-span-2 row-span-1 bg-wh-500'></div>
-                    <div className='col-span-1 row-span-1 bg-wh-500'></div>
-                    <div className='col-span-1 row-span-1 bg-wh-500'></div>
+                    <TrendingCard className='col-span-2 row-span-2 bg-wh-500'></TrendingCard>
+                    <TrendingCard className='col-span-2 row-span-1 bg-wh-500'></TrendingCard>
+                    <TrendingCard className='col-span-1 row-span-1 bg-wh-500'></TrendingCard>
+                    <TrendingCard className='col-span-1 row-span-1 bg-wh-500'></TrendingCard>
 
                 </div>
         </section>
