5648dcb3e2593302f3173b0513470964976cf8d5
bovdaboat <ignatgabov@gmail.com>
2023-05-31 22:00:20 -0700

    added a class name to a file

--- app/(home)/Travel.tsx
+++ app/(home)/Travel.tsx
@@ -4,7 +4,16 @@ type Props = {}
 
 const Travel = () => {
     return (
-        <div>Travel</div>
+        <section className="mt-3">
+            <hr className="border-1" />
+            <div className="flex items-center gap-3 my-8">
+                <h4 className="bg-accent-green   py-2 px-5 text-wh-900 text-sm font-bold">
+                    Travel
+                </h4>
+                <p className="font-bold text-2xl">New travel Experience</p>
+            </div>
+        </section>
+
     )
 }
 
