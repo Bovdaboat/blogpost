e3447c4c2adfa5974536d1256425482022693b78
bovdaboat <ignatgabov@gmail.com>
2023-06-01 22:12:02 -0700

    added a sidebar fuction

---
+++ app/(shared)/Sidebar.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+type Props = {}
+
+const Sidebar = () => {
+    return (
+        <section>Sidebar</section>
+    )
+}
+
+export default Sidebar
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Tech from "@/app/(home)/Tech";
 import Travel from "@/app/(home)/Travel"
 import Other from "@/app/(shared)/Other";
 import Subscribe from "@/app/(shared)/Subscribe";
+import Sidebar from "@/app/(shared)/Sidebar";
 export default function Home() {
   return (
     <main className="px-10 leading-7">
@@ -18,7 +19,7 @@ export default function Home() {
                 </div>
             </div>
             <div className="basis-1/4">
-
+                    <Sidebar />
             </div>
         </div>
     </main>
