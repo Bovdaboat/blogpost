63514e6da8fed9d862b199e6257b28d8997e0ab8
bovdaboat <ignatgabov@gmail.com>
2023-06-01 22:29:53 -0700

    adding the meet and bones to the file Sidebar.tsx

--- app/(shared)/Sidebar.tsx
+++ app/(shared)/Sidebar.tsx
@@ -1,11 +1,30 @@
 import React from "react"
+import SocialLinks from "@/app/(shared)/SocialLinks";
+import Subscribe from "@/app/(shared)/Subscribe";
 
 
 type Props = {}
 
 const Sidebar = () => {
     return (
-        <section>Sidebar</section>
+        <section>
+            <h4 className="bg-wh-900 py-3 px-5 text-wh-50 text-xs font-bold text-center">
+                Subscribe and Follow
+            </h4>
+            <div className="my-5 mx-5">
+                <SocialLinks isDark />
+            </div>
+            <Subscribe />
+            <div className="bg-wh-900">
+                    advert image
+            </div>
+            <h4 className="bg-wh-900 py-3 px-5 text-wh-50 text-xs font-bold text-center">
+                about the blog
+            </h4>
+            <div className="bg-wh-900">
+                advert image
+            </div>
+        </section>
     )
 }
 
