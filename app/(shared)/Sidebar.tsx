51cbba70b9b733ee01c12fba1f9c3f4b68ad6648
bovdaboat <ignatgabov@gmail.com>
2023-06-01 22:37:33 -0700

    finished doing the Sidebar.tsx and finished the file

--- app/(shared)/Sidebar.tsx
+++ app/(shared)/Sidebar.tsx
@@ -1,11 +1,13 @@
-import React from "react"
-import SocialLinks from "@/app/(shared)/SocialLinks";
-import Subscribe from "@/app/(shared)/Subscribe";
+import Image from "next/image";
+import React from "react";
+import SocialLinks from "./SocialLinks";
+import Subscribe from "./Subscribe";
+import Ad2 from "/public/assets/ad-2.png";
+import AboutProfile from "/public/assets/about-profile.jpg";
 
+type Props = {};
 
-type Props = {}
-
-const Sidebar = () => {
+const Sidebar = (props: Props) => {
     return (
         <section>
             <h4 className="bg-wh-900 py-3 px-5 text-wh-50 text-xs font-bold text-center">
@@ -15,17 +17,34 @@ const Sidebar = () => {
                 <SocialLinks isDark />
             </div>
             <Subscribe />
-            <div className="bg-wh-900">
-                    advert image
-            </div>
+            <Image
+                className="hidden md:block my-8 w-full"
+                alt="advert-2"
+                placeholder="blur"
+                src={Ad2}
+                width={500}
+                height={1000}
+            />
             <h4 className="bg-wh-900 py-3 px-5 text-wh-50 text-xs font-bold text-center">
-                about the blog
+                About the Blog
             </h4>
-            <div className="bg-wh-900">
-                profile image
+            <div className="flex justify-center my-3">
+                <Image
+                    alt="about-profile"
+                    placeholder="blur"
+                    src={AboutProfile}
+                    style={{ width: "500px", height: "250px", objectFit: "cover" }}
+                />
             </div>
+            <h4 className="py-3 px-5 text-wh-500 font-bold text-center">
+                Geoffrey Epstein
+            </h4>
+            <p className="text-wh-500 text-center text-sm">
+                Sit diam vel lacus tortor molestie amet tincidunt. Amet amet arcu sed
+                facilisi
+            </p>
         </section>
-    )
-}
+    );
+};
 
-export default Sidebar
+export default Sidebar;
