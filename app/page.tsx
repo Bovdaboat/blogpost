a1130dd70ab5f80d4762d2a27899b0bdfa6bc1e3
bovdaboat <ignatgabov@gmail.com>
2023-06-03 22:35:35 -0700

    added and asick and added the file for package.json

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,25 @@ import Travel from "@/app/(home)/Travel"
 import Other from "@/app/(shared)/Other";
 import Subscribe from "@/app/(shared)/Subscribe";
 import Sidebar from "@/app/(shared)/Sidebar";
+
+const getPosts = async () => {
+    const posts = await prisma.post.findMany();
+
+    const formattedPosts = await Promise.all(
+        posts.map(async (post: Post) => {
+            const imageModule = require(`../public${post.image}`);
+            return {
+                ...post,
+                image: imageModule.default,
+            };
+        })
+    );
+
+    return formattedPosts;
+};
+
+
+
 export default function Home() {
   return (
     <main className="px-10 leading-7">
